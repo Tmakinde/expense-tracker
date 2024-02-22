178836c723ce95d7e90a68fd2a69f5d03ff9c01b
tmakinde <tolulope.makinde@internetbrands.com>
2024-02-22 19:34:22 +0100

    Add database migration files

---
+++ database/migrations/create_expenses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExpensesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_type')->nullable();
+            $table->bigInteger('user_id');
+            $table->foreignId('category_id');
+            $table->foreign('category_id')->on('categories')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string('amount');
+            $table->string('currency', 3);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
---
+++ database/migrations/create_user_limits_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserLImitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_limits', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_type')->nullable();
+            $table->bigInteger('user_id');
+            $table->foreignId('category_id');
+            $table->foreign('category_id')->on('categories')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string('limit_type');
+            $table->string('amount');
+            $table->string('currency', 3);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
