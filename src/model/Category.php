64354b9b8b03ce489564d9070d0fb20f6ad9d5e8
tmakinde <tolulope.makinde@internetbrands.com>
2024-05-13 07:54:42 +0100

    Work on categoty limit relationship

--- src/model/Category.php
+++ src/model/Category.php
@@ -4,11 +4,18 @@ namespace Tmakinde\ExpenseTracker\Model;
 use Tmakinde\ExpenseTracker\Model\Expense;
 
 use Illuminate\Database\Eloquent\Model;
-class Category extends Model {
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Tmakinde\ExpenseTracker\Trait\InteractsWithUserLimit;
+
+class Category extends Model
+{
+    use InteractsWithUserLimit;
 
     protected $guarded = ['id'];
 
-    public function expenses() {
+    public function expenses() : BelongsTo
+    {
         return $this->belongsTo(Expense::class);
     }
+
 }
