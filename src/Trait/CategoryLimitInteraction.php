a071cc598c18f82ed1eeb823c6f3e7bc24fed068
tmakinde <tolulope.makinde@internetbrands.com>
2024-06-24 12:54:34 +0100

    Validate user enum limit type

--- src/Trait/CategoryLimitInteraction.php
+++ src/Trait/CategoryLimitInteraction.php
@@ -3,6 +3,7 @@ namespace Tmakinde\ExpenseTracker\Trait;
 
 use Exception;
 use Illuminate\Database\Eloquent\Model;
+use Tmakinde\ExpenseTracker\Enum\LimitType;
 use Tmakinde\ExpenseTracker\Model\UsersLimits;
 
 trait CategoryLimitInteraction
@@ -16,11 +17,22 @@ trait CategoryLimitInteraction
         return $this;
     }
 
+    private function validateEnumType(string $limitType) : bool
+    {
+        $enum = LimitType::getAllTypes();
+        return !in_array($limitType, $enum) ? false : true;
+    }
+
     public function createLimit(string $limitType, float $amount) : UsersLimits
     {
         if (!isset($this->userLimit->user_id)) {
             throw new Exception('Set user id for limit');
         }
+
+        if ($this->validateEnumType($limitType)) {
+            throw new Exception('Invalid limit type');
+        }
+
         $this->userLimit->limit_type = $limitType;
         $this->userLimit->amount = $amount;
         $this->userLimit->currency = data_get(app()->configPath('expense_tracker_config'), 'currency');
